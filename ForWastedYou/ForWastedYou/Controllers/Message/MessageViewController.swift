e83d6fc1ce8de8e4f7cf778e32807575d09b9a44
D1438 <daisuke.workin@gmail.com>
2021-12-16 07:49:47 +0900

    addTextボタンを追加

--- ForWastedYou/ForWastedYou/Controllers/Message/MessageViewController.swift
+++ ForWastedYou/ForWastedYou/Controllers/Message/MessageViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class MessageViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var addTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addTextButton.setTitle("addText", for: .normal)
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
