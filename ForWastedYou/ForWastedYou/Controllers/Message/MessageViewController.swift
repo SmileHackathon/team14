709d41ad11539b10e2bece7fb8be5ff1bd83f324
I.D <daisuke.workin@gmail.com>
2021-12-16 15:52:46 +0900

    Update MessageViewController.swift

--- ForWastedYou/ForWastedYou/Controllers/Message/MessageViewController.swift
+++ ForWastedYou/ForWastedYou/Controllers/Message/MessageViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
-class MessageViewController: UIViewController {
-
+class MessageViewController: UIViewController, UITextFieldDelegate {
+    
+    var messageFields: [UITextField]?
+    
+    
+    @IBOutlet weak var addTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addTextButton.setTitle("addText", for: .normal)
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func TappedAddTextButton(_ sender: Any) {
+        
+        let unitWidth = view.frame.width
+        let unitHeight = view.frame.height
+        
+        let messageField: UITextField = UITextField(frame: CGRect(x: unitWidth/2, y: unitHeight/2, width: 50.0, height: 50.0))
+        messageField.delegate = self
+        messageField.placeholder = "入力してください。"
+        messageField.keyboardType = UIKeyboardType.default
+        messageFields?.append(messageField)
+        self.view.addSubview(messageField)
+        print("作ったよ")
     }
-    */
-
+    
+    
 }
